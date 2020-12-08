29cd570f4dc8f4954e8dd5d82c7c3cf526e9993c
Hector Zhao <zzhao495@marshall.usc.edu>
2020-12-08 03:06:47 -0800

    Update Douban Crawler.py

--- Douban Crawler.py
+++ Douban Crawler.py
@@ -12,29 +12,29 @@ headers = {'origin':'https://y.qq.com',
 # Most parameters are just from copy&paste, only change value of "p" so that I can get as many pages as I want. 
 for num in range (1,3):
     params = {'ct': '24',
-'qqmusic_ver': '1298',
-'remoteplace': 'txt.yqq.lyric',
-'searchid': '96529858210962475',
-'aggr': '0',
-'catZhida': '1',
-'lossless': '0',
-'sem': '1',
-'t': '7',
-# Using for loop to change the number of pages
-'p': str(num),
-'n': '5',
-# Singer name
-'w': '周杰伦',
-'g_tk_new_20200303': '5381',
-'g_tk': '5381',
-'loginUin': '0',
-'hostUin': '0',
-'format': 'json',
-'inCharset': 'utf8',
-'outCharset': 'utf-8',
-'notice': '0',
-'platform': 'yqq.json',
-'needNewCode': '0'}
+            'qqmusic_ver': '1298',
+            'remoteplace': 'txt.yqq.lyric',
+            'searchid': '96529858210962475',
+            'aggr': '0',
+            'catZhida': '1',
+            'lossless': '0',
+            'sem': '1',
+            't': '7',
+            # Using for loop to change the number of pages
+            'p': str(num),
+            'n': '5',
+            # Singer name
+            'w': '周杰伦',
+            'g_tk_new_20200303': '5381',
+            'g_tk': '5381',
+            'loginUin': '0',
+            'hostUin': '0',
+            'format': 'json',
+            'inCharset': 'utf8',
+            'outCharset': 'utf-8',
+            'notice': '0',
+            'platform': 'yqq.json',
+            'needNewCode': '0'}
 
     # Initiate request, include headers and params defined above
     res = requests.get(url, headers = headers, params= params)
