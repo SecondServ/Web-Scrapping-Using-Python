45be7a9f6cb9036e7a7360c6d0ff6f67b4f0160e
Hector Zhao <zzhao495@marshall.usc.edu>
2020-12-08 18:34:21 -0800

    Update Automated Browser.py

--- Automated Browser.py
+++ Automated Browser.py
@@ -1,20 +1,34 @@
 from selenium import webdriver
 import time
 
-
+# Set browser to be Chrome
 driver = webdriver.Chrome()
+# Access targeted url
 driver.get('https://wordpress-edu-3autumn.localprod.oc.forchange.cn/wp-login.php')
-user = driver.find_element_by_id('user_login')
+# Wait 3 second for browser to open the page
 time.sleep(3)
-user.send_keys('agreezhao@gmail.com')
+# Locate the place for inputing username
+user = driver.find_element_by_id('user_login')
+# Input username
+user.send_keys('xxxxx@gmail.com')
+# Locate the place for inputing password
 pas = driver.find_element_by_id('user_pass')
-pas.send_keys('zzh960607')
+# Input username
+pas.send_keys('xxxxxxxx')
+# Loacte 'submit' button 
 sub = driver.find_element_by_id('wp-submit')
+# Click 'submit' button
 sub.click()
+# Wait 2 second for browser to jump
 time.sleep(2)
+# Locate the link I want to click
 article = driver.find_element_by_link_text('未来已来（三）——同九义何汝秀')
+# Click
 article.click()
+# Locate the place for inputing comment
 comment = driver.find_element_by_name('comment')
+# Input comment
 comment.send_keys('selenium比cookie好用得多啊！')
+# Locate and click 'submit' button
 post = driver.find_element_by_id('submit')
 post.click()
